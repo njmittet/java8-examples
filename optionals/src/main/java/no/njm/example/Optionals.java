db718b77d643d0f5bc6ebaf70c1efe63fc7fb3c7
Nils Jørgen Mittet <njmittet@gmail.com>
2014-06-20 09:50:54 +0200

    Add Optional examples

---
+++ optionals/src/main/java/no/njm/example/Optionals.java
@@ -0,0 +1,96 @@
+package no.njm.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+/**
+ * An Optional is a class that encapsulates an optional value, and can be viewed as
+ * a single-value container that either contains a value or doesn't.
+ * <p>
+ * Ufortunatley
+ * Due to backwards-compability, none of the existing Java collections will be retrofitted into returning Optionals.
+ * <pre>
+ * List<MyObject> list = getList();
+ * Optional<E> optional = list.get(index);
+ * </pre>
+ */
+public class Optionals {
+
+    private static final Logger log = LoggerFactory.getLogger(Optionals.class);
+
+    Optional<String> upperCaseFirstLetter(String source) {
+        if (source.isEmpty()) {
+            return Optional.empty();
+        }
+        String uppercased = upperCase(firstLetter(source)) + lowerCase(allButFirstLetter(source));
+        return Optional.of(uppercased);
+    }
+
+    Optional<String> wrapNull() {
+        return Optional.ofNullable(null);
+    }
+
+    private String upperCase(String source) {
+        return source.toUpperCase();
+    }
+
+    private String firstLetter(String source) {
+        return source.substring(0, 1);
+    }
+
+    private String lowerCase(String source) {
+        return source.toLowerCase();
+    }
+
+    private String allButFirstLetter(String source) {
+        return source.substring(1);
+    }
+
+    public static void main(String[] args) {
+        Optionals optionals = new Optionals();
+        plainOptional(optionals);
+        lambdaOptional(optionals);
+        fallbackOptional(optionals);
+        nullWraped(optionals);
+        filterOptional(optionals);
+        mapOptional(optionals);
+    }
+
+    private static void plainOptional(Optionals optionals) {
+        Optional<String> optional = optionals.upperCaseFirstLetter("source");
+        if (optional.isPresent()) {
+            log.debug("Optional is {}", optional.get());
+        }
+    }
+
+    private static void lambdaOptional(Optionals optionals) {
+        Optional<String> optional = optionals.upperCaseFirstLetter("source");
+        optional.ifPresent((s) -> log.debug(s));
+    }
+
+    private static void fallbackOptional(Optionals optionals) {
+        Optional<String> optional = optionals.upperCaseFirstLetter("");
+        log.debug(optional.orElse("Default"));
+    }
+
+    private static void nullWraped(Optionals optionals) {
+        Optional<String> optional = optionals.wrapNull();
+        log.debug(optional.orElse("Default"));
+    }
+
+    private static void filterOptional(Optionals optionals) {
+        Optional<String> res = optionals.upperCaseFirstLetter("source");
+        res.filter(s -> s.length() > 4).ifPresent(s -> log.debug(s));
+    }
+
+    private static void mapOptional(Optionals optionals) {
+        Optional<String> optional = optionals.upperCaseFirstLetter("source");
+        log.debug("String length is {}", optional.map(Optionals::countLetters).get());
+    }
+
+    static int countLetters(String input) {
+        return input.length() - 1;
+    }
+}
