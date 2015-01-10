01ff2bb96e109ad417186bb458017b5d8c03eb79
Nils Jørgen Mittet <njmittet@gmail.com>
2015-01-10 17:56:28 +0100

    Add module Streams
    Added module without examples.

---
+++ streams/src/main/java/no/njm/example/Streams.java
@@ -0,0 +1,15 @@
+package no.njm.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ */
+public class Streams {
+
+    private static final Logger log = LoggerFactory.getLogger(Streams.class);
+
+    public static void main(String[] args) {
+
+    }
+}
