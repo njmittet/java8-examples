ae9fb2e4cc5ccbbd8a8042efd1ad676aeef234ef
Nils Jørgen Mittet <njmittet@gmail.com>
2015-01-10 18:00:19 +0100

    Add module DateApi
    Added module without examples.

---
+++ date-api/src/main/java/no/njm/example/DateApi.java
@@ -0,0 +1,15 @@
+package no.njm.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ */
+public class DateApi {
+
+    private static final Logger log = LoggerFactory.getLogger(DateApi.class);
+
+    public static void main(String[] args) {
+
+    }
+}
